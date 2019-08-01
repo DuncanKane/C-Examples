0f90472247c5a55ad586a31d405c3424036fab5e
DuncanKane <47971814+DuncanKane@users.noreply.github.com>
2019-08-01 12:26:32 +0100

    Add files via upload

---
+++ Mini-Max Sum/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr)
+    {
+        long sumOfNumbers = 0;
+        long iMax = 0;
+        long iMin = Int64.MaxValue;
+
+        foreach (int iNumber in arr)
+        {
+            sumOfNumbers += iNumber;
+            if (iNumber < iMin)
+            {
+                iMin = iNumber;
+            }
+            if (iNumber > iMax)
+            {
+                iMax = iNumber;
+            }
+        }
+        Console.WriteLine("{0} {1}", sumOfNumbers - iMax, sumOfNumbers - iMin);
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        miniMaxSum(arr);
+    }
+}
---
+++ palindrome/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class Palindrome
+{
+    public static bool IsPalindrome(string word)
+    {
+        word = word.ToLower();
+        string newWord = word;
+        char[] newArr = newWord.ToCharArray();
+
+        Array.Reverse(newArr);
+
+        Console.WriteLine(newArr);
+
+        string tempWord = new string (newArr);
+                
+        if (tempWord == word)
+        {
+            return true;
+        }
+        
+        
+        throw new NotImplementedException("Waiting to be implemented.");
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Palindrome.IsPalindrome("Deleveled"));
+        Console.ReadLine();
+    }
+}
